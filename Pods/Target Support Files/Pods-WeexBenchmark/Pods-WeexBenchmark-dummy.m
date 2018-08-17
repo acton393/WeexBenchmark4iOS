95121f1aaa1c8c09230cfb331f65a91db168f2e1
zifan.zx <zifan.zx@alibaba-inc.com>
2018-08-17 11:12:19 +0800

    Revert "init update"
    This reverts commit 4bb31a27046010b257c918b94f9714ab5141608d.

--- Pods/Target Support Files/Pods-WeexBenchmark/Pods-WeexBenchmark-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WeexBenchmark : NSObject
-@end
-@implementation PodsDummy_Pods_WeexBenchmark
-@end
--- Pods/Target Support Files/Pods-WeexBenchmarkUITests/Pods-WeexBenchmarkUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WeexBenchmarkUITests : NSObject
-@end
-@implementation PodsDummy_Pods_WeexBenchmarkUITests
-@end
